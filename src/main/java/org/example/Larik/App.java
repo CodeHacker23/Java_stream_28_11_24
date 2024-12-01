6e30ece167e1468d7db6a491feb840ef1b344adf
Квантор <ilarion0@yandex.ru>
2024-12-01 03:04:38 +0300

    Коментарий 1 мск :)

--- src/main/java/org/example/Larik/App.java
+++ src/main/java/org/example/Larik/App.java
@@ -15,13 +15,13 @@ public class App{
     public static void main(String[] args) {
        AdFilm adFilm = new AdFilm();
 
-//        adFilm.films().stream() //Отфильтровать фильмы, уже выпущенные  (использовать filter). и вывести
-//               .filter(film -> film.getLocalDate().isBefore(LocalDate.now()))
-//               .forEach(System.out::println);
-//
-//       adFilm.films().stream() // отфильтровать фильмы, еще не  выпущенные  (использовать filter). и вывести
-//               .filter(film -> film.getLocalDate().isAfter(LocalDate.now()))
-//               .forEach(System.out::println);
+        adFilm.films().stream() //Отфильтровать фильмы, уже выпущенные  (использовать filter). и вывести
+               .filter(film -> film.getLocalDate().isBefore(LocalDate.now()))
+               .forEach(System.out::println);
+
+       adFilm.films().stream() // отфильтровать фильмы, еще не  выпущенные  (использовать filter). и вывести
+               .filter(film -> film.getLocalDate().isAfter(LocalDate.now()))
+               .forEach(System.out::println);
 
         adFilm.films().stream()
                 .filter(film -> film.getLocalDate().isAfter(LocalDate.now()))
