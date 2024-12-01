9bfd2c666972a1c17774b9c01f3efa00d2937791
Квантор <ilarion0@yandex.ru>
2024-12-01 03:14:22 +0300

    Комментарий 2 мск :)

--- src/main/java/org/example/Larik/App.java
+++ src/main/java/org/example/Larik/App.java
@@ -16,14 +16,14 @@ public class App{
        AdFilm adFilm = new AdFilm();
 
         adFilm.films().stream() //Отфильтровать фильмы, уже выпущенные  (использовать filter). и вывести
-               .filter(film -> film.getLocalDate().isBefore(LocalDate.now()))
+               .filter(film -> film.getLocalDate().isBefore(LocalDate.now())) // метод isBefore  -Он возвращает true, если вызывающий экземпляр хронологически раньше указанного времени, в противном случае — false
                .forEach(System.out::println);
 
        adFilm.films().stream() // отфильтровать фильмы, еще не  выпущенные  (использовать filter). и вывести
-               .filter(film -> film.getLocalDate().isAfter(LocalDate.now()))
+               .filter(film -> film.getLocalDate().isAfter(LocalDate.now())) // метод isAfter - проверяет, находится ли текущий экземпляр после указанного. то бишь сравнивает текущую дату реального времени и проверяет прошла ли эта дата или нет
                .forEach(System.out::println);
 
-        adFilm.films().stream()
+        adFilm.films().stream() //Отфильтровать фильмы, еще не  выпущенные  (использовать filter). и вывести получить новый лист где будут только фильмы цена билета которых выше 1000 руб
                 .filter(film -> film.getLocalDate().isAfter(LocalDate.now()))
                 .filter(film -> film.getPrise() > 1000.0)
                 .collect(Collectors.toCollection(HashSet::new))
